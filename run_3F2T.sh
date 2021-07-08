fb55141bd5c99f0bdd4e70259ac3df6904a0f823
yoda yuanzhe <yuanzhezhou@gmail.com>
2021-07-08 14:49:55 -0500

    add command line option -c

--- run_3F2T.sh
+++ run_3F2T.sh
@@ -1,1 +1,1 @@
-./RLDOCK -i Examples/3F2T/3F2T_RNA.mol2 -l Examples/3F2T/3F2T_ligand.mol2 -o Examples/3F2T/3F2T -c 10 -n 4 -s src/sphere.dat -r Examples/3F2T/3F2T_ref_lig.mol2
+./RLDOCK -i Examples/3F2T/3F2T_RNA.mol2 -l Examples/3F2T/3F2T_ligand.mol2 -o Examples/3F2T/3F2T -c 10 -n 30 -s src/sphere.dat -r Examples/3F2T/3F2T_ref_lig.mol2
